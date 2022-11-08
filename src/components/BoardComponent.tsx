9160641556fb958fe045a3a53f7214ff88623940
Егор Гординский <yegorgordinskiy2002@gmail.com>
2022-11-08 15:13:18 +0800

    add the output of the board to the screen

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { FC } from "react";
+import { Board } from "../models/Board";
+import CellComponent from "./CellComponent";
 
-function BoardComponent() {
-  return <div>BoardComponent</div>;
+interface BoardComponentProps {
+  board: Board;
+  setBoard: (board: Board) => void;
 }
+
+const BoardComponent: FC<BoardComponentProps> = ({ board, setBoard }) => {
+  return (
+    <div className="board">
+      {board.cells.map((row, index) => (
+        <React.Fragment key={index}>
+          {row.map((cell) => (
+            <CellComponent key={cell.id} cell={cell} />
+          ))}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
 
 export default BoardComponent;
